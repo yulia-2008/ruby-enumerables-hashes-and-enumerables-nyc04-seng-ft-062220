a9933a052767b86170e6d5d2f9b36e85bbc064d7
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-21 20:27:35 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,7 +8,8 @@
 
 def select_winner(passengers)
   passengers.each do |key, value|
-    key==suite_a && value.start_with?("A")
+    if key==suite_a && value.start_with?("A")
     winner=value
+end
 end
 end
